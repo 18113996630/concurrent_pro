20063975a9e1bf32238a4022bd852e792c445432
hr <18113996630>
2019-03-07 16:43:07 +0800

    init

---
+++ src/test/java/com/hrong/concurrent_pro/ConcurrentProApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hrong.concurrent_pro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrentProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
